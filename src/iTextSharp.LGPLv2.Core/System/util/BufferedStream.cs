bd84322b647eae2f47058771c352960da75975ff
VahidN <vahid_nasiri@yahoo.com>
2017-06-08 10:55:25 +0430

    Added netstandard2.0 support

--- src/iTextSharp.LGPLv2.Core/System/util/BufferedStream.cs
+++ src/iTextSharp.LGPLv2.Core/System/util/BufferedStream.cs
@@ -1,3 +1,4 @@
+#if NETSTANDARD1_3
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
@@ -1129,3 +1130,4 @@ namespace System.IO
         }
     }  // class BufferedStream
 }  // namespace
+#endif
