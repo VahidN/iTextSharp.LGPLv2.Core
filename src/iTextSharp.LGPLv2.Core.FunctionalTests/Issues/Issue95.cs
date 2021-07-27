324da8cb9c607a8237408c3ee5459f157f647081
Adrian C <adrian.cearnau@xconta.ro>
2021-07-27 15:52:45 +0300

    #95: serialize the element itself when preparing the XFA form data

--- src/iTextSharp.LGPLv2.Core.FunctionalTests/Issues/Issue95.cs
+++ src/iTextSharp.LGPLv2.Core.FunctionalTests/Issues/Issue95.cs
@@ -15,19 +15,24 @@ namespace iTextSharp.LGPLv2.Core.FunctionalTests.Issues
     [TestClass]
     public class Issue95
     {
+        public const string XmlRootElementName = "form1";
+        public const string XmlChildElementName = "childElement";
+
         public const string XmlDeclarationFragment = "<?xml version=";
+        public const string XmlElementRootFragment = "<form1";
 
         [TestMethod]
         public void Verify_Issue95_XfaForm_SerializeDoc_Does_Not_Add_Xml_Declaration()
         {
             var xmlDoc = new XmlDocument();
 
-            var form1 = xmlDoc.CreateElement("form1");
+            var form1 = xmlDoc.CreateElement(XmlRootElementName);
             xmlDoc.AppendChild(form1);
 
-            form1.AppendChild(xmlDoc.CreateElement("subObject"));
-            form1.AppendChild(xmlDoc.CreateElement("subObject"));
-            form1.AppendChild(xmlDoc.CreateElement("subObject"));
+            // Add some child elements
+            form1.AppendChild(xmlDoc.CreateElement(XmlChildElementName));
+            form1.AppendChild(xmlDoc.CreateElement(XmlChildElementName));
+            form1.AppendChild(xmlDoc.CreateElement(XmlChildElementName));
 
             var bytes = XfaForm.SerializeDoc(xmlDoc);
             var text = Encoding.UTF8.GetString(bytes);
@@ -38,7 +43,15 @@ namespace iTextSharp.LGPLv2.Core.FunctionalTests.Issues
                 XmlDeclarationFragment, 
                 StringComparison.InvariantCultureIgnoreCase) == 0;
 
+            // We should have the root element at the beginning of the serialized text ; this also
+            // makes sure that we're not mstakenly adding the UTF-8 identifier to the beginning of
+            // the serialized text
+            var startsWithRootelement = text.IndexOf(
+                XmlElementRootFragment,
+                StringComparison.InvariantCulture) == 0;
+
             Assert.IsFalse(containsXmlDeclaration);
+            Assert.IsTrue(startsWithRootelement);
         }
     }
 }
