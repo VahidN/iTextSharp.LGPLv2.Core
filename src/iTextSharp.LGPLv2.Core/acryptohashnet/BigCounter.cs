9d544c4cf41610b3105a64aa315e8df104f41166
Michael Cheers <michaelcheers8@gmail.com>
2022-09-14 12:06:53 -0700

    Fixed issues with MD5 not working in WebAssembly

---
+++ src/iTextSharp.LGPLv2.Core/acryptohashnet/BigCounter.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace acryptohashnet
+{
+    public sealed class BigCounter
+    {
+        private readonly int sizeInBytes;
+        private readonly uint[] array;
+
+        public BigCounter(int sizeInBytes)
+        {
+            if (sizeInBytes < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(sizeInBytes));
+            }
+
+            this.sizeInBytes = sizeInBytes;
+
+            var sizeInUints = sizeInBytes / 4;
+            if (sizeInBytes % 4 > 0)
+            {
+                sizeInUints += 1;
+            }
+
+            array = new uint[sizeInUints];
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] result = new byte[sizeInBytes];
+
+            Buffer.BlockCopy(array, 0, result, 0, result.Length);
+
+            return result;
+        }
+
+        public void Clear()
+        {
+            for(int ii = 0; ii < array.Length; ii++)
+            {
+                array[ii] = 0;
+            }
+        }
+
+        public void Add(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value));
+            }
+
+            Add((uint)value);
+        }
+
+        public void Add(uint value)
+        {
+            Add(0, value);
+        }
+
+        public void Add(long value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value));
+            }
+
+            Add(0, (uint)value);
+            Add(1, (uint)(value >> 32));
+        }
+
+        public uint ToUInt32()
+        {
+            return array[0];
+        }
+
+        public ulong ToULong()
+        {
+            return (((ulong)array[1]) << 32) + array[0];
+        }
+
+        private void Add(int index, uint value)
+        {
+            if (index >= array.Length)
+            {
+                throw new OverflowException("Counter is not big enough");
+            }
+
+            var maxAllowedValue = uint.MaxValue - array[index];
+            if (value > maxAllowedValue)
+            {
+                array[index] = value - (maxAllowedValue + 1);
+                Add(index + 1, 1);
+            }
+            else
+            {
+                array[index] += value;
+            }
+        }
+    }
+}
---
+++ src/iTextSharp.LGPLv2.Core/acryptohashnet/BlockHashAlgorithm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace acryptohashnet
+{
+    /// <summary>
+    /// Represents the base class from which all implementation of block hash algorithms must derive.
+    /// </summary>
+    public abstract class BlockHashAlgorithm : HashAlgorithm
+    {
+        private readonly byte[] lastBlock;
+
+        private int lastBlockLength;
+
+        /// <summary>
+        /// Block hash algorithm ctor.
+        /// </summary>
+        /// <param name="blockSize">size of the block for algorithm in bytes</param>
+        public BlockHashAlgorithm(int blockSize)
+        {
+            BlockSize = blockSize;
+
+            lastBlock = new byte[BlockSize];
+            lastBlockLength = 0;
+        }
+
+        /// <summary>
+        /// Size of algorithm block in bytes.
+        /// </summary>
+        public int BlockSize { get; }
+
+        /// <summary>
+        /// Initialization algorithm variables.
+        /// </summary>
+        public override void Initialize()
+        {
+            Array.Clear(lastBlock, 0, lastBlock.Length);
+            lastBlockLength = 0;
+        }
+
+        /// <summary>
+        /// Processing block of bytes (size is @BlockSize), @array length must be >= than @offset + @BlockSize
+        /// </summary>
+        /// <param name="array">array of bytes</param>
+        /// <param name="offset">offset from begin of block in @array</param>
+        protected abstract void ProcessBlock(byte[] array, int offset);
+
+        /// <summary>
+        /// Processing final block of bytes (size is @length), @array length must be >= than @offset + @length
+        /// </summary>
+        /// <param name="array">array of bytes</param>
+        /// <param name="offset">offset from begin of block in @array</param>
+        /// <param name="length">length of final block</param>
+        protected abstract void ProcessFinalBlock(byte[] array, int offset, int length);
+
+        /// <summary>
+        /// Resulting value of algorithm
+        /// </summary>
+        /// <value>byte array with hash value</value>
+        protected abstract byte[] Result { get; }
+
+        /// <summary>
+        /// Main hash procedure.
+        /// </summary>
+        /// <param name="array">byte array</param>
+        /// <param name="offset">offset in array</param>
+        /// <param name="length">length of block for processing</param>
+        protected override void HashCore(byte[] array, int offset, int length)
+        {
+            if (lastBlockLength > 0)
+            {
+                int lastBlockRemaining = BlockSize - lastBlockLength;
+                if (length >= lastBlockRemaining)
+                {
+                    Buffer.BlockCopy(array, offset, lastBlock, lastBlockLength, lastBlockRemaining);
+
+                    ProcessBlock(lastBlock, 0);
+                    offset += lastBlockRemaining;
+                    length -= lastBlockRemaining;
+
+                    lastBlockLength = 0;
+                }
+            }
+
+            while (length >= BlockSize)
+            {
+                ProcessBlock(array, offset);
+                offset += BlockSize;
+                length -= BlockSize;
+            }
+
+            if (length > 0)
+            {
+                Buffer.BlockCopy(array, offset, lastBlock, lastBlockLength, length);
+                lastBlockLength += length;
+            }
+        }
+
+        /// <summary>
+        /// Hash final block.
+        /// </summary>
+        /// <returns>hash value</returns>
+        protected override byte[] HashFinal()
+        {
+            if (lastBlockLength > lastBlock.Length)
+            {
+                throw new InvalidOperationException("lastBlockLength > lastBlock.Length");
+            }
+
+            ProcessFinalBlock(lastBlock, 0, lastBlockLength);
+            return Result;
+        }
+    }
+}
